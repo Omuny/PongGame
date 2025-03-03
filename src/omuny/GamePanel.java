e6d342db0d55328548d13ffa5f8ae85297b003cd
omuny <omuny@yandex.ru>
2025-03-03 16:33:01 +0300

    Создание скелета для класса GamePanel. Наполнение базовыми методами

--- src/omuny/GamePanel.java
+++ src/omuny/GamePanel.java
@@ -1,4 +1,62 @@
 package omuny;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 public class GamePanel {
+
+    GamePanel() {
+
+    }
+
+    // Создание нового мяча
+    public void newBall() {
+
+    }
+
+    // Создание нового весла
+    public void newPaddles() {
+
+    }
+
+    // Рисование предметов на экране
+    public void paint(Graphics g) {
+
+    }
+
+    // Отрисовка экрана
+    public  void draw(Graphics g) {
+
+    }
+
+    // Перемещение элементов
+    public void move() {
+
+    }
+
+    // Проверка столкновений
+    public void checkCollision() {
+
+    }
+
+    // Запуск
+    public void run() {
+
+    }
+
+    // Класс для прослушивания действий
+    public class AL extends KeyAdapter {
+        // Ожидание нажатия клавиш
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        // Реализация нажатия клавиш
+        public void keyReleased(KeyEvent e) {
+
+        }
+    }
+
 }
