136b2e45d1a248bae598951205dc7ba0e4d5009f
omuny <omuny@yandex.ru>
2025-03-03 16:43:54 +0300

    Создание скелета для класса Score. Наполнение базовыми методами

--- src/omuny/Score.java
+++ src/omuny/Score.java
@@ -1,4 +1,19 @@
 package omuny;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 public class Score {
+
+    Score() {
+
+    }
+
+    // Отрисовка результата
+    public void draw(Graphics g) {
+
+    }
+
 }
