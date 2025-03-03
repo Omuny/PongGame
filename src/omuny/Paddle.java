2fa1614384d4c47dd8f567b5013652c558907081
omuny <omuny@yandex.ru>
2025-03-03 16:37:56 +0300

    Создание скелета для класса Paddle. Наполнение базовыми методами

--- src/omuny/Paddle.java
+++ src/omuny/Paddle.java
@@ -1,4 +1,39 @@
 package omuny;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 public class Paddle {
+
+    Paddle() {
+
+    }
+
+    // Нажатие клавиши
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    // Реализация клавиш
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    // Установка вертикального положения весла
+    public void setYDirection(int yDirection) {
+
+    }
+
+    // Передвижение
+    public void move() {
+
+    }
+
+    // Отрисовка
+    public void draw(Graphics g) {
+
+    }
+
 }
