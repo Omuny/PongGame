181e09edc89adf859fa899beef78e1638e5f61c9
omuny <omuny@yandex.ru>
2025-03-03 16:40:58 +0300

    Создание скелета для класса Ball. Наполнение базовыми методами

--- src/omuny/Ball.java
+++ src/omuny/Ball.java
@@ -1,4 +1,29 @@
 package omuny;
 
+import java.awt.*;
+
 public class Ball {
+
+    Ball() {
+
+    }
+
+    // Установка направления по X
+    public void setXDirection(int randomXDirection) {
+
+    }
+
+    // Установка направления по Y
+    public void setYDirection(int randomYDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
 }
