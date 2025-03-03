31f36c776f719680fe033038c1aa5194f6fb5630
omuny <omuny@yandex.ru>
2025-03-03 20:35:00 +0300

    Реализуем передвижение вёсел при нажатии клавиш

--- src/omuny/Paddle.java
+++ src/omuny/Paddle.java
@@ -7,8 +7,9 @@ import javax.swing.*;
 
 public class Paddle extends Rectangle {
 
-    int id;
+    int id; // id весла: 1 - первый игрок, 2 - второй игрок
     int yVelocity;
+    int speed = 10; // Скорость весел
 
     Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
         super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
@@ -17,22 +18,60 @@ public class Paddle extends Rectangle {
 
     // Нажатие клавиши
     public void keyPressed(KeyEvent e) {
-
+        switch (id) {
+            case 1:
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(-speed);
+                    move();
+                } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+            case 2:
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(-speed);
+                    move();
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+        }
     }
 
     // Реализация клавиш
     public void keyReleased(KeyEvent e) {
-
+        switch (id) {
+            case 1:
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(0);
+                    move();
+                } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+            case 2:
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(0);
+                    move();
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+        }
     }
 
     // Установка вертикального положения весла
     public void setYDirection(int yDirection) {
-
+        yVelocity = yDirection;
     }
 
     // Передвижение весла
     public void move() {
-
+        y += yVelocity;
     }
 
     // Отрисовка весла
